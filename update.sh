2d180b82e1e9afb977f1fb3fd0c235c3545e95b4
Kenji Saito <ken-yo@mbr.nifty.com>
2023-10-03 21:28:54 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -19,7 +19,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-if ! pnpm install -r && pnpm up -r && pnpm -r build ; then
+if ! (pnpm install -r && pnpm up -r && pnpm -r build) ; then
   cd "${CUR}" || exit
   exit $result
 fi

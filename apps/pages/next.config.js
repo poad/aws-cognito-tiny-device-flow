ac7c069e8ba05cee64d684fbd3300d014dc9caa6
Kenji Saito <ken-yo@mbr.nifty.com>
2025-01-18 16:18:06 +0900

    Bumps node modules

--- apps/pages/next.config.js
+++ apps/pages/next.config.js
@@ -1,18 +1,15 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 
 /** @type {import('next').NextConfig} */
 const config = {
   output: "export",
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    esmExternals: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = withBundleAnalyzer(config);
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(config);
--- apps/pages/postcss.config.js
+++ apps/pages/postcss.config.js
@@ -1,7 +1,5 @@
-module.exports = {
+export default {
     plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+      "@tailwindcss/postcss": {},
     },
   }
-  

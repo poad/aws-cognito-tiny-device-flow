eb1223467737442e6d78166a7c790252a48a837f
Kenji Saito <ken-yo@mbr.nifty.com>
2025-01-23 17:31:56 +0900

    Bumps node modules

--- apps/pages/next.config.js
+++ apps/pages/next.config.js
@@ -6,10 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const config = {
   output: "export",
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    esmExternals: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },

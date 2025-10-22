a6d1414fccfbce87c3d6e98782554801db9e6bfa
Kenji Saito <ken-yo@mbr.nifty.com>
2025-10-23 00:21:22 +0900

    Bumps node modules

--- apps/pages/eslint.config.js
+++ apps/pages/eslint.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import react from 'eslint-plugin-react';
@@ -8,7 +9,6 @@ import globals from 'globals';
 import nextPlugin from '@next/eslint-plugin-next';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
-// @ts-ignore
 import importPlugin from 'eslint-plugin-import';
 import flowtypePlugin from 'eslint-plugin-flowtype';
 import pluginPromise from 'eslint-plugin-promise'
@@ -26,7 +26,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-export default tseslint.config(
+export default defineConfig(
   includeIgnoreFile(gitignorePath),
   {
     ignores: [
@@ -42,9 +42,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
-  ...compat.config({
-    extends: ['next/core-web-vitals'],
-  }),
+  // @ts-ignore
   pluginPromise.configs['flat/recommended'],
   {
     files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
@@ -80,8 +78,6 @@ export default tseslint.config(
     },
     plugins: {
       '@stylistic': stylistic,
-      '@stylistic/ts': stylistic,
-      '@stylistic/jsx': stylistic,
       react,
       'jsx-a11y': jsxA11yPlugin,
       '@next/next': nextPlugin,
@@ -89,10 +85,6 @@ export default tseslint.config(
       'react-hooks': reactHooksPlugin,
     },
     extends: [
-      // @ts-ignore
-      ...compat.config(jsxA11yPlugin.configs.recommended),
-      ...compat.config(jsxA11yPlugin.configs.recommended),
-
       ...tseslint.configs.strict,
       ...tseslint.configs.stylistic,
     ],
@@ -105,17 +97,13 @@ export default tseslint.config(
       'react/no-direct-mutation-state': 'off',
       'react/no-string-refs': 'off',
       'react/jsx-no-undef': 'off',
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      ...reactHooksPlugin.configs.recommended.rules,
       '@next/next/no-duplicate-head': 'off',
       '@next/next/no-img-element': 'error',
       '@next/next/no-page-custom-font': 'off',
       'import/namespace': 'off',
       'import/no-named-as-default': 'off',
       'import/no-named-as-default-member': 'off',
-      '@stylistic/ts/indent': ['error', 2],
-      '@stylistic/jsx/jsx-indent': ['error', 2],
+      '@stylistic/indent': ['error', 2],
       '@stylistic/semi': ['error', 'always'],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
       '@stylistic/arrow-parens': ['error', 'always'],
